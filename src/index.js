12bf97ecacf470d43e00e5c6cb2485716ad382ef
Md. Abu Taher <entrptaher@gmail.com>
2019-03-25 05:04:46 +0600

    remove useless files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import Navigo from "navigo";
 import renderPage from './page';
 
-// Initial Navigo
-var root = null;
-var useHash = true; // Defaults to: false
-var hash = "#!"; // Defaults to: '#'
-var router = new Navigo(root, useHash, hash);
+var router = new Navigo();
 
 router
   .on({
